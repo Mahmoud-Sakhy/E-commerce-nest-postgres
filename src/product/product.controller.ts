2cc44be669381699080535d009215a7cf927f6f7
Mahmoud-Sakhy <mahmoudsakhyanwer@gmail.com>
2026-02-12 21:12:01 +0200

    update: latest changes

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -8,22 +8,6 @@ export class ProductsController {
 
   @Get('/api/products')
   getAllProducts() {
-    return [
-      {
-        id: 1,
-        title: 'Product 1',
-        price: 100,
-      },
-      {
-        id: 2,
-        title: 'Product 2',
-        price: 200,
-      },
-      {
-        id: 3,
-        title: 'Product 3',
-        price: 300,
-      },
-    ];
+    return [];
   }
 }
